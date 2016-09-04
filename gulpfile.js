cd9676d641735eaa6dd139db8340e486b83298fd
markus <markjoseph.solidarios@outlook.com>
2016-09-04 22:26:05 +0800

    Added documentation header

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,19 @@ const browserSync = require('browser-sync').create();
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const gutil = require('gulp-util');
+const ghead = require('gulp-header');
+const fs = require('fs');
 
 const siteRoot = '_site';
 const cssFiles = '_css/**/*.?(s)css';
+const pkg = require('./package.json');
 
-gulp.task('css', () => {
-  gulp.src(cssFiles)
-    .pipe(concat('all.css'))
-    .pipe(gulp.dest('assets'));
-});
+// gulp.task('css', () => {
+//   gulp.src(cssFiles)
+//     .pipe(concat('all.css'))
+//     .pipe(gulp.dest('css'));
+// });
+
 
 gulp.task('jekyll', () => {
   const jekyll = child.spawn('jekyll', ['build',
@@ -31,6 +35,13 @@ gulp.task('jekyll', () => {
   jekyll.stderr.on('data', jekyllLogger);
 });
 
+//Add documentation header
+gulp.task('header', () => {
+  gulp.src('_site/index.html')
+    .pipe(ghead(fs.readFileSync('_includes/doc.html', 'utf8'), { pkg : pkg }))
+    .pipe(gulp.dest('_site/'))
+});
+
 gulp.task('serve', () => {
   browserSync.init({
     files: [siteRoot + '/**'],
@@ -39,8 +50,6 @@ gulp.task('serve', () => {
       baseDir: siteRoot
     }
   });
-
-  gulp.watch(cssFiles, ['css']);
 });
 
-gulp.task('default', ['css', 'jekyll', 'serve']);
+gulp.task('default', ['jekyll', 'header', 'serve']);
