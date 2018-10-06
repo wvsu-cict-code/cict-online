0bbc59262bef118c4674d16b9f4c838b6b02db3d
ninariccimarie <ninariccimarie@gmail.com>
2018-10-06 23:13:40 +0800

    add default message and promptSymbol

--- src/pages/GeekMode.js
+++ src/pages/GeekMode.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
 import ReactTerminal from 'react-terminal-component'
+import {
+  EmulatorState, OutputFactory,
+  Outputs,
+} from 'javascript-terminal'
 
 class GeekMode extends Component {
   render() {
+    const defaultState = EmulatorState.createEmpty();
+    const defaultOutputs = defaultState.getOutputs();
+
+    const newOutputs = Outputs.addRecord(
+      defaultOutputs, OutputFactory.makeTextOutput(
+        `Welcome to Geek Mode! You can browse CICT Online using
+        commands. For a list of available commands, execute help`
+      )
+    );
+    const emulatorState = defaultState.setOutputs(newOutputs);
     return (
-      <ReactTerminal />
+      <ReactTerminal
+        inputStr=''
+        promptSymbol='~ guest#'
+        emulatorState={emulatorState}
+      />
     )
   }
 }
