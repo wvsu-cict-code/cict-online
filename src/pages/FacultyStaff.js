19137ac2b723cc2c359c1f590f6a293db19257c0
bashdev7 <bashdev7@gmail.com>
2018-10-13 23:36:45 +0800

    Code cleanup for faculty and staff page

--- src/pages/FacultyStaff.js
+++ src/pages/FacultyStaff.js
@@ -7,6 +7,124 @@ import Typist from 'react-typist'
 import Navbar from '../components/Navbar';
 import DefaultContainer from '../components/DefaultContainer';
 
+const connector = (name, x,y, i, o) => {
+        return {
+            title: name,
+            ports: [
+                { name: '('+i+')', type: 'in' },
+                { name: '('+o+')', type: 'out' },
+            ],
+            color: '#EC1261',
+            position: [x, y]
+        }
+}
+
+const nodes = [
+    {
+        title: '(1) Dr. Joel T. De Castro',
+        ports: [{ name: 'Dean (1)', type: 'out' }],
+        color: '#FFED18',
+        position: [100, 100]
+    },
+        connector('(2) +', 338, 144, 2, 3),
+    {
+        title: '(3) Dr. Arnel N. Secondes',
+        ports: [{ name: 'College Secretary (4)', type: 'in' }],
+        color: '#1565C0',
+        position: [471,49]
+    },
+    {
+        title: '(4) Dr. Luche Sabayle',
+        ports: [{ name: 'Associate Dean (5)', type: 'in' }],
+        color: '#1565C0',
+        position: [472,249]
+    },
+    connector('(5) +', 689.4629104616998,136.6771159874604, 6, 7),  
+    {
+        title: '(6) Ms. Jenibem Gantala',
+        ports: [{ name: 'Graduate School Clerk / Technician (8)', type: 'in' }],
+        color: '#333',
+        position: [734,18]
+    },
+        {
+        title: '(7) Ms. Marian P. Figueroa',
+        ports: [{ name: 'Admin Aide Clerk (9)', type: 'in' }],
+        color: '#333',
+        position: [725,295]
+    },  
+    connector('(8) +', 856.4629104616998,150.6771159874604, 10, 11),
+    connector('(9) Coordinators', 1001.4629104616998,70.67711598746041, 13, 12),
+    connector('(10) College Divisions', 972.4629104616998,257.6771159874604, 15, 14),
+            {
+        title: '(11) Dr. Bobby D. Gerardo',
+        ports: [{ name: 'VPAA, Graduate School Coordinator (16)', type: 'in' }],
+        color: '#1565C0',
+        position: [1168.3821075795604,148.13196786106622]
+    }, 
+     {
+        title: '(12) Dr. Frank Elijorde',
+        ports: [{ name: 'Research Coordinator (17)', type: 'in' }],
+        color: '#1565C0',
+        position: [1365.8000180273216,222.62610330240636]
+    }, 
+    {
+        title: '(13) Prof. Cyreneo Dofitas Jr.',
+        ports: [{ name: 'Research Coordinator (18)', type: 'in' }],
+        color: '#1565C0',
+        position: [1476.8000180273216,144.62610330240636]
+    }, 
+        {
+        title: '(14) Mr. Evans Sansolis',
+        ports: [        
+        { name: '(20)', type: 'out' },
+        { name: 'Computer Lab Supervisor (19)', type: 'in' },
+        ],
+        color: '#1565C0',
+        position: [1642.8000180273216,210.62610330240636]
+    },
+            {
+        title: '(15) Mr. Evan Sumido',
+        ports: [{ name: 'Computer Lab Supervisor (21)', type: 'in' }],
+        color: '#1565C0',
+        position: [1780.7701672810529,144.04401375016755]
+    },
+    {
+        title: '(16) Mr. Felizardo Ygot',
+        ports: [
+        { name: 'Computer Lab Personnel (22)', type: 'in' },
+        { name: '(23)', type: 'out' },
+        ],
+        color: '#1565C0',
+        position: [1746.8000180273216,302.62610330240636]
+    },
+        {
+        title: '(17) Mr. Russel Laurence Ferrer',
+        ports: [{ name: 'Computer Lab Personnel (24)', type: 'in' }],
+        color: '#1565C0',
+        position: [1838.8000180273216,399.62610330240636]
+    },
+]
+
+const connections = [
+    [0,1],
+    [2,3],
+    [2,4],
+    [2,5],
+    [6,7],
+    [6,8],
+    [5,10],
+    [10,13],
+    [10,11],
+    [12,15],
+    [12,16],
+    [12,17],
+    [19,12],
+    [12,20],
+    [21, 18],
+    [22,23]
+
+]
+
 const Typer = text => {
     console.log(text)
     return (
@@ -36,60 +154,45 @@ class FacultyStaff extends Component {
 
     componentDidMount() {
         const app = this
-        var node1 = new DefaultNodeModel("Dr. Joel T. De Castro", "#FFED18");
-        let port1 = node1.addOutPort("Dean");
-        node1.setPosition(100, 200);
+        const parts = []
+        const ports = []
+        const links = []
+        Object.keys(nodes).map(i => {
+            parts.push(new DefaultNodeModel(nodes[i].title, nodes[i].color))
+        })
 
-        var cnode1 = new DefaultNodeModel("", "#EC1261");
-        let cport1 = cnode1.addInPort(" ");
-        let cport2 = cnode1.addOutPort(" ");
-        let cport3 = cnode1.addOutPort(" ");
-        cnode1.setPosition(300, 200);
+        Object.keys(parts).map(i => {
+            parts[i].setPosition(nodes[i].position[0], nodes[i].position[1])
+        })
 
-        var cnode2 = new DefaultNodeModel("", "#EC1261");
-        let cport4 = cnode2.addInPort(" ");
-        let cport5 = cnode2.addOutPort(" ");
-        let cport6 = cnode2.addOutPort(" ");
-        cnode2.setPosition(600, 200);
+        Object.keys(parts).map(i => {
+            let port = null
+            nodes[i].ports.map(j => {
+                console.log("ports added")
+                if(j.type==='in'){
+                    port = parts[i].addInPort(j.name)
+                }else{
+                    port = parts[i].addOutPort(j.name)                    
+                }
+            ports.push(port)  
+            })     
+        })
 
-        var node2 = new DefaultNodeModel("Dr. Arnel N. Secondes", "#1565C0");
-        let port2 = node2.addInPort("College Secretary");
-        node2.setPosition(400, 100);
+        parts.map(i => {
+            this.model.addNode(i)
+        })
 
-        var node3 = new DefaultNodeModel("Dr. Ma. Luche Sabayle", "#1565C0");
-        let port3 = node3.addInPort("Associate Dean");
-        node3.setPosition(400, 300);
+        connections.map(i => {
+            const link = ports[i[0]].link(ports[i[1]])
+            app.model.addLink(link)
+        })
 
-        let link1 = cport2.link(port2);
-        let link2 = cport3.link(port3);
-        let link3 = port1.link(cport1);
-        let link4 = cport4.link(cport2);
-
-        let nodes = [node1, cnode1, node2, node3];
-
-        this.model.addAll(
-            node1,
-            node2,
-            cnode1,
-            cnode2,
-            node3,
-            port1,
-            port2,
-            port3,
-            cport1,
-            cport2,
-            cport3,
-            link1,
-            link2,
-            link3,
-            link4,
-        )
-
-        nodes.forEach(node => {
+        parts.forEach(node => {
             node.addListener({
-                selectionChanged: e => {
+                selectionChanged: e => {                    
                     if (e.isSelected) {
-                        if (e.entity.name !== "") {
+                        console.log(e.entity.x+","+e.entity.y)
+                        if (e.entity.name !== "+") {
                             app.showInfo(e)
                         }
                     } else {
@@ -99,7 +202,6 @@ class FacultyStaff extends Component {
             })
         })
 
-        // 7) load model into engine
         this.engine.setDiagramModel(this.model);
 
     }
