124774b86d43ae488d0785643b005f647117404d
mjsolidarios <bashdev7@gmail.com>
2020-05-12 18:18:43 +0800

    Social links update

--- src/components/SocialSection.tsx
+++ src/components/SocialSection.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Facebook, GitHub, Twitter } from "react-feather";
+import { Facebook, GitHub, Twitter, Mail } from "react-feather";
 import { Button } from 'antd';
 
 const socialLinks = [
@@ -21,11 +21,17 @@ const socialLinks = [
         icon: <GitHub className="mx-auto my-2" />,
         link: "https://github.com/wvsu-cict-code"
     },
+    {
+        id: "m",
+        name: "Email",
+        icon: <Mail className="mx-auto my-2" />,
+        link: "mailto:admission.cict@wvsu.edu.ph"
+    },
 ]
 
 const SocialSection: React.FC = () => (
     <div className="text-center p-4 text-sm" style={{backgroundColor: '#FCFCFC'}}>
-        <p className="mb-2">Follow Us</p>
+        <p className="mb-2">Get Connected!</p>        
         {socialLinks.map(i => (
             <Button key={i.id} className="mx-2" href={i.link} target="_blank" shape="circle" size="large">{i.icon}</Button>
         ))}
