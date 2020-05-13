43e24932e36e605182adfc5397badb5fa0d543cc
mjsolidarios <bashdev7@gmail.com>
2020-05-13 17:38:53 +0800

    WVSU link fix

--- src/components/Footerbar.tsx
+++ src/components/Footerbar.tsx
@@ -16,7 +16,7 @@ const Footerbar = (props: themeProps) => (
                     <span className={`font-normal ${props.themeState == 'base' ? 'text-gray-700' : 'text-gray-300'}`}>
                         CICT Online ©2020 by CICTzens | <Link target="_blank" to="https://github.com/wvsu-cict-code/cict-online">Contribute to Code</Link> | <Link to="https://spectrum.chat/cict-online" target="_blank">Join in Spectrum</Link>
                     </span>
-                    <p className="mt-2">
+                    <p className={`mt-2 ${props.themeState == 'base' ? 'text-gray-700' : 'text-gray-300'}`}>
                         <Link to="https://wvsu.edu.ph/" target="_blank">West Visayas State University</Link>
                     </p>
                 </div>
