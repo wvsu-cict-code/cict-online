1c9d20ee24387bf64769931b793566d35091b87b
Mark Joseph Solidarios <mjsolidarios@wvsu.edu.ph>
2021-02-07 14:45:14 +0800

    Update volunteers.tsx

--- src/pages/volunteers.tsx
+++ src/pages/volunteers.tsx
@@ -93,7 +93,7 @@ const volunteers: {
             avatar: lumbao,
             name: "Mark Anthony Lumbao",
             work: ["Volunteer ReactJS Developer", "Dynamic Theming Implementor", "Geek Mode Developer"],
-            link: "https://m-a-l.herokuapp.com/"
+            link: "https://mark-lumbao.github.io/"
         },
         {
             avatar: malimit,
